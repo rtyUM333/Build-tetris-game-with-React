1d6445fe113aeb571a4e5bd0e09c426c1c5c0dad
RP <rp@RPdeMacBook-Pro-2.local>
2024-04-02 16:33:17 -0500

    updated

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+import { createStage } from "../gameHelpers";
+
 
 //components
 import Stage from "./Stage";
@@ -11,7 +13,7 @@ const Tetris = () => {
 
     return (
         <div>
-            <Stage />
+            <Stage stage={createStage()}/>
             <aside>
                 <div>
                     <Display text="Score" />
