ddbb9ffc78b7949ad2c11afd0702d131c55a7f3e
RP <rp@RPdeMacBook-Pro-2.local>
2024-04-02 16:11:53 -0500

    starting a new project

---
+++ src/components/Cell.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { StyledCell } from "./styles/StyledCell";
+import { TETROMINOS } from "../tetrosminos";
+
+
+const Cell = ({ type }) => (
+    <StyledCell type={'L'} color={TETROMINOS['L'].color}>cell</StyledCell>
+)
+
+export default Cell; 
