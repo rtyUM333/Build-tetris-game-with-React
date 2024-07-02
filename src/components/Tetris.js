267b310908ef5d739f5ca0c80b6ecf51bacde747
RP <rp@RPdeMacBook-Pro-2.local>
2024-07-02 11:33:11 -0500

    random tetrominos showing up

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { useState } from "react";
 
+import { createStage } from '../gameHelpers';
+
 
 // styled components
 import { StyledTetrisWrapper, StyledTetris } from "./styles/StyledTetris";
@@ -21,11 +23,45 @@ const Tetris = () => {
     const [dropTime, setDropTime] = useState(null);
     const [gameOver, setGameOver] = useState(false);
 
-    const [player] = usePlayer();
+    const [player, updatePlayerPos, resetPlayer] = usePlayer();
     const [ stage, setStage] = useStage(player);
 
+
+    const movePlayer = dir => {
+        updatePlayerPos({ x: dir, y:0});
+    }
+
+    const startGame = () => {
+        // reset everything
+        setStage(createStage());
+        resetPlayer();
+    }
+
+    const drop = () => {
+        updatePlayerPos({ x:0, y:1, collided: false })
+    }
+
+    const dropPlayer = () => {
+        drop();
+    }
+
+
+    const move = ({ keyCode }) => {
+        if (!gameOver) {
+            if (keyCode === 37) {
+                movePlayer(-1);
+            } else if (keyCode === 39) {
+                movePlayer(1);
+            } else if (keyCode === 40) {
+                dropPlayer();
+            }
+        }
+    }
+
+
+
     return (
-        <StyledTetrisWrapper>
+        <StyledTetrisWrapper role="button" tabIndex="0" onKeyDown={ e => move(e)}> 
             <StyledTetris>
             <Stage stage={stage}/>
             <aside>
@@ -38,7 +74,7 @@ const Tetris = () => {
                     <Display text="Level" />
                 </div>
             )}
-                <StartButton />
+                <StartButton callback={startGame} />
             </aside>
             </StyledTetris>
         </StyledTetrisWrapper>
--- src/hooks/usePlayer.js
+++ src/hooks/usePlayer.js
@@ -1,6 +1,7 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 
 import { randomTetromino } from '../tetrosminos';
+import { STAGE_HEIGHT } from "../gameHelpers";
 
 export const usePlayer = () => {
     const [player, setPlayer] = useState({
@@ -9,7 +10,21 @@ export const usePlayer = () => {
         collided: false, 
     });
 
-    return [player];
+    const updatePlayerPos = ({ x, y, collided}) => {
+        setPlayer(prev => ({
+            ...prev, 
+            pos: {x: (prev.pos.x += x), y: (prev.pos.y += y)}, 
+            collided,
+        }))
+    }
 
+    const resetPlayer = useCallback (() => {
+        setPlayer({
+            pos: { x: STAGE_HEIGHT / 2 - 2, y:0 },
+            tetromino: randomTetromino().shape,
+            collided: false,
+        })
+    }, [])
 
+    return [player, updatePlayerPos, resetPlayer];
 }
